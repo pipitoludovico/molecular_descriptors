d4a1c2cf54a4e6aaf664c4c0a71748158a7dbb83
Ludovico PIpitò <67153855+pipitoludovico@users.noreply.github.com>
2022-08-13 09:19:10 +0100

    Add files via upload
    fixed bug where wrong pdbs were not canonicalized (not even attempted at)

--- clean_smiles.py
+++ clean_smiles.py
@@ -1,5 +1,4 @@
 from rdkit import Chem
-from rdkit.Chem import rdmolops
 
 from dataParser import *
 
--- dataParser.py
+++ dataParser.py
@@ -1,9 +1,8 @@
 import subprocess
 import sys
-
+from rdkit import Chem
+from rdkit.Chem import Draw
 import pandas as pd
-from biopandas.pdb import PandasPdb
-from clean_smiles import *
 
 
 class Parser:
@@ -13,11 +12,22 @@ class Parser:
         if data.endswith('.csv'):
             self.data = pd.read_csv(data)
         if data.endswith('.pdb'):
-            self.obabel_result = subprocess.check_output("obabel -i pdb " + sys.argv[1] + " -o smi", shell=True,
-                                                         text=True)
-            self.pdbRep = {}
-            self.pdbRep['SMILES'] = str(self.obabel_result).split()[0]
-            self.data = pd.DataFrame([self.pdbRep])
+            try:
+                self.obabel_result = subprocess.check_output("obabel -i pdb " + sys.argv[1] + " -o smi", shell=True,
+                                                             text=True)
+                self.pdbRep = {}
+                self.pdbRep['SMILES'] = str(self.obabel_result).split()[0]
+                self.draw = Chem.MolFromSmiles(str(self.obabel_result).split()[0])
+                self.data = pd.DataFrame([self.pdbRep])
+                Draw.MolToFile(self.draw, f"{str(sys.argv[1]).replace('.pdb', '.png')}")
+            except:
+                self.mol = Chem.MolFromPDBFile(data)
+                self.smileFromMol = Chem.MolToSmiles(self.mol)
+                self.pdbRep = {}
+                self.pdbRep['SMILES'] = self.smileFromMol
+                self.data = pd.DataFrame([self.pdbRep])
+                Draw.MolToFile(self.mol, f"{str(sys.argv[1]).replace('.pdb', '.png')}")
+
         if data.endswith('.xlml'):
             self.data = pd.read_excel(data)
 
